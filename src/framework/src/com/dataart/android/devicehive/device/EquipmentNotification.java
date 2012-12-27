a68f0f482523597b8a12d1d3c0eda1d803cd17ea
mkiselev <mkiselev@e5115c30-f446-4c2b-b752-175e35e92961>
2012-12-27 12:03:11 +0000

    Android framework: ability to get command by its id. EquipmentNotification parameters layout fix.
    git-svn-id: https://svn.dataart.com/svn/embedded.cloud/trunk@826 e5115c30-f446-4c2b-b752-175e35e92961

--- src/framework/src/com/dataart/android/devicehive/device/EquipmentNotification.java
+++ src/framework/src/com/dataart/android/devicehive/device/EquipmentNotification.java
@@ -6,35 +6,29 @@ import com.dataart.android.devicehive.Notification;
 
 /**
  * Represents a {@link Notification} which is usually sent by an
- * {@link Equipment}.
+ * {@link Equipment} to update its state.
  */
 public class EquipmentNotification extends Notification {
 
 	/**
 	 * Construct a notification with given equipment code and additional
-	 * equipment parameters dictionary and it's name inside {@link Notification}
-	 * 's parameters dictionary.
+	 * equipment parameters dictionary.
 	 * 
 	 * @param equipmentCode
 	 *            Equipment code.
-	 * @param parametersName
-	 *            Equipment parameters dictionary name inside
-	 *            {@link Notification}'s parameters dictionary.
-	 * @param equipmentParameters
+	 * @param parameters
 	 *            Equipment parameters dictionary.
 	 */
-	public EquipmentNotification(String equipmentCode, String parametersName,
-			HashMap<String, Object> equipmentParameters) {
-		super("equipment", equipmentParameters(equipmentCode, parametersName,
-				equipmentParameters));
+	public EquipmentNotification(String equipmentCode,
+			HashMap<String, Object> parameters) {
+		super("equipment", equipmentParameters(equipmentCode, parameters));
 	}
 
 	private static HashMap<String, Object> equipmentParameters(
-			String equipmentCode, String parametersName,
-			HashMap<String, Object> equipmentParameters) {
-		HashMap<String, Object> parameters = new HashMap<String, Object>();
+			String equipmentCode, HashMap<String, Object> parameters) {
+		final HashMap<String, Object> equipmentParameters = new HashMap<String, Object>(
+				parameters);
 		parameters.put("equipment", equipmentCode);
-		parameters.put(parametersName, equipmentParameters);
-		return parameters;
+		return equipmentParameters;
 	}
 }
